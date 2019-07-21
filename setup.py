48b0b58952a8ff8aa4fd391ca6c7b1d0e688098a
Skippy <not@anemail.com>
2019-07-20 21:12:03 -0400

    Fixed packages in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="bob",
     version="0.9.0",
     author="skippy",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://127.0.0.1",
-    packages=['bob'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
